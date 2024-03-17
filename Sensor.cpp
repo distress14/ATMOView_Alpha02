e949fddedbcadf5a4342e34c805875c4b7e60a3e
Andrea <110169272+distress14@users.noreply.github.com>
2024-03-17 12:39:01 +0100

    Linux vs & Class editing

---
+++ Sensor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include "Sensor.h"
+
+int const MAX_XPOS = 50;
+int const MAX_YPOS = 50;
+
+bool InputSanification(float User_XPOS, float User_YPOS)
+{
+    if ((User_XPOS < MAX_XPOS) || (User_YPOS > MAX_YPOS)){ return 0;}  //Checks if the Max height/weight of the "map" is respected. Return 0 if OutOfBonds
+};
+
+
+bool Sensor::NewSensor(float posX, float posY)
+{
+    if (InputSanification(posX, posY) == 0){    //Calls the [TODO bool InputSanification function], which returns True if the Input is respected, 0 otherwise
+        std::cout << "Something went wrong with the Input. Error: 001 " << std::endl;   //Error 001 - Errors will be "sequential", so I can track easly where the error is
+        return 0;      //Exit the program. TODO --> Display Error on the "alert" window and recall a NEW "alert" window, asking gently if you can choose between 0-5
+    }
+    return 1;
+};
---
+++ Sensor.h
@@ -0,0 +1,25 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+class Sensor
+{
+private:
+    const float posX, posY;    //Using const cause the position will be fixed once selected, and float (32bit) instead of double (64 bit)
+    const int Identification;   //ID-Identification will not change once selected.
+    const int SensorType;   //Needs to be 0-4 (max 5 Sensors). It will be the ID for the Sensor type choosed.  //TODO SANIFICATION of INPUT
+    int LastUpdate;     // Keep tracking time of last update of the XML file (if used)
+    float SensorValue;         //Actual value of the Sensor (Not used int cause I wanna the PRECISION)
+    float SensorValueMax;    //If needed, a bottom value (es:temperature)
+    float SensorValueMin;   //If needed, a maximum value (es:temperature)
+    float LastSensorValue;    //It can be used to do the comparation PRE-ACTUAL in (+/-)%
+    const bool FromXML;    //Bool const which tells me if the User choosed to use an XML file. 0: Not Used | 1: Used -> Do not start Simulation for that Sensor
+
+public:
+    Sensor();  //First Default-Constructor
+    ~Sensor();   //First Destructor
+    bool NewSensor(posX, posY); //Adds 1 new Sensor to the SXSideSensor tab. Need to POP-UP an "alert" (New window) in which must be added ID - SensorType - FromXML. Remainings are optional
+    bool DeleteSensor();   //Otherwise delete it. For now kept on bool type to check if everthing went ok  --> converting to void??
+};
+
+
+#endif // SENSOR_H
